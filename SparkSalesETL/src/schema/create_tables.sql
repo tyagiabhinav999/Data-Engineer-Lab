dbf59ed6ef203dbbad46a467b48ec65961cc7523
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2025-04-06 22:46:45 +0530

    add schema ddl for tables

---
+++ SparkSalesETL/src/schema/create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE product_staging_table (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    file_name VARCHAR(255),
+    file_location VARCHAR(255),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    status VARCHAR(1)
+);
+
