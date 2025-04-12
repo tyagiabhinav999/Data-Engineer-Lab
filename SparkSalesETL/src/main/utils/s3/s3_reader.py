47bda496cf24ea0c17bf219246e216f68a8a11ea
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2025-04-12 21:09:21 +0530

    add comment to show total available files and capitalize Key in s3 reader

--- SparkSalesETL/src/main/utils/s3/s3_reader.py
+++ SparkSalesETL/src/main/utils/s3/s3_reader.py
@@ -4,10 +4,12 @@ class S3Reader:
 
     def list_files(self, s3_client, bucket_name, folder_path):
         try:
-            response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=folder_path)
+            response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=f"{folder_path}/")
             if 'Contents' in response:
-                files = [f"s3://{bucket_name}/{obj['key']}" for obj in response['Contents'] if
-                         not obj['key'].endswith('/')]
+                print(f"Total files available in folder {folder_path} of bucket {bucket_name}: {response}")
+                files = [f"s3://{bucket_name}/{obj['Key']}" for obj in response['Contents'] if
+                         not obj['Key'].endswith('/')]
+
                 return files
             else:
                 return []
