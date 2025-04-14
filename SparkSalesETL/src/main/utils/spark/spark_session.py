e61eca0268c3035cfd550166ebb4b09fe409177f
tyagiabhinav999 <tyagiabhinav999@gmail.com>
2025-04-14 17:56:22 +0530

    update spark session with jar path

--- SparkSalesETL/src/main/utils/spark/spark_session.py
+++ SparkSalesETL/src/main/utils/spark/spark_session.py
@@ -1,10 +1,24 @@
 import findspark
 findspark.init()
 from pyspark.sql import SparkSession
+from pathlib import Path
 
 def spark_session():
-    spark = SparkSession.builder.master("local[*]") \
-        .appName("SparkSalesETL")\
-        .config("spark.driver.extraClassPath", f"C:\mysql-jar\mysql-connector-j-9.2.0") \
+    # Path to the jar directory
+    jar_dir = Path(r"C:\mysql-jar")
+
+    # Find the first .jar file in the folder
+    jar_files = list(jar_dir.glob("*.jar"))
+
+    if not jar_files:
+        raise FileNotFoundError(f"No .jar files found in {jar_dir}")
+
+    jar_path = str(jar_files[0])  # Convert to string for Spark config
+
+    spark = SparkSession.builder \
+        .master("local[*]") \
+        .appName("SparkSalesETL") \
+        .config("spark.jars", jar_path) \
         .getOrCreate()
+
     return spark
